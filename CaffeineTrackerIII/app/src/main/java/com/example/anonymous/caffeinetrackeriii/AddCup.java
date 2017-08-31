da6e65a68ab465e14f8273f2e4ee12cb771ef072
ANONYMOUS <iamcicada3301@gmail.com>
2017-08-30 20:52:27 -0700

    modified caffeine tracker III

--- CaffeineTrackerIII/app/src/main/java/com/example/anonymous/caffeinetrackeriii/AddCup.java
+++ CaffeineTrackerIII/app/src/main/java/com/example/anonymous/caffeinetrackeriii/AddCup.java
@@ -1,10 +1,13 @@
 package com.example.anonymous.caffeinetrackeriii;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +18,8 @@ public class AddCup extends AppCompatActivity implements AdapterView.OnItemSelec
 
     Spinner beverage_spinner;
     Spinner coffeeSpinner;
+    EditText quantity;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +28,39 @@ public class AddCup extends AppCompatActivity implements AdapterView.OnItemSelec
 
         beverage_spinner = (Spinner) findViewById(R.id.beverage_spinner);
         coffeeSpinner = (Spinner) findViewById(R.id.teaCoffee);
+        quantity = (EditText) findViewById(R.id.quantity);
+        submit = (Button) findViewById(R.id.submit);
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.beverage, android.R.layout.simple_spinner_item);
 
         beverage_spinner.setAdapter(adapter);
         beverage_spinner.setOnItemSelectedListener(this);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(beverage_spinner.getSelectedItem().equals("Type Of Beverage") && coffeeSpinner.getSelectedItem().equals("None")){
+
+                    Toast.makeText(getApplicationContext(), "PLEASE MAKE SURE ALL THE REQUIRED FIELDS ARE FILLED!!!", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    String packetType = coffeeSpinner.getSelectedItem().toString();
+                    String packetQuantity = quantity.getText().toString();
+                    Intent toShowAdded = new Intent(getApplicationContext(), Added.class);
+                    toShowAdded.putExtra("PACKET", packetType);
+                    toShowAdded.putExtra("QUANTITY", packetQuantity);
+                    startActivity(toShowAdded);
+
+                }
+            }
+        });
     }
 
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        
+
         if (beverage_spinner.getSelectedItem().equals("Coffee")) {
 
             ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this,
@@ -49,5 +76,6 @@ public class AddCup extends AppCompatActivity implements AdapterView.OnItemSelec
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
 
